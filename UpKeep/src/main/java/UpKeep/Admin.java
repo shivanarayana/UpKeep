b4aa908ee1b52072baf76addd67f60c318dba54d
shivanarayana <shhivaa00@gmail.com>
2021-03-23 00:35:36 +0530

    fifth commit
    Entity manager using persistence in JPA

---
+++ UpKeep/src/main/java/UpKeep/Admin.java
@@ -0,0 +1,28 @@
+package UpKeep;
+
+import UpKeep.DAO.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public
+class Admin {
+    public static void main(String args[]){
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("jpa-tutorial");
+        EntityManager em = emf.createEntityManager();
+
+        User user = em.find(User.class,51L);
+//        Transaction steps
+
+//        em.getTransaction().begin();
+//
+//        em.persist(a); em.persist(p); em.persist(c);
+//
+//        em.getTransaction().commit();
+
+        System.out.println(user);
+
+    }
+}
--- UpKeep/src/main/java/UpKeep/Controller/ParkingController.java
+++ UpKeep/src/main/java/UpKeep/Controller/ParkingController.java
@@ -4,6 +4,7 @@ import UpKeep.DAO.Parking;
 import UpKeep.DAO.User;
 import UpKeep.Repository.ParkingRepository;
 import UpKeep.Repository.UserRepository;
+
 import UpKeep.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,9 @@ public
 class ParkingController {
 
     @Autowired
+    UserService userServiceParking;
+
+    @Autowired
     ParkingRepository parkingRepository;
 
     @Autowired
@@ -27,12 +31,13 @@ class ParkingController {
     public Status registerSpace(@Valid @RequestBody Parking park) {
 
         List<User> userList = park.getParkuser();
+
         for (User user : userList) {
-            if (userparkRepository.findById(user.getId()) == null) {
-                userparkRepository.save(user);
-            }
-            Parking mypark = parkingRepository.findById(park.getParkid()).orElse(null);
-            mypark.setVehicletype(park.getVehicletype());
+            user = userServiceParking.searchUser(user);
+            if ( user !=null)
+            {
+            Parking mypark = parkingRepository.findById(park.getParkid()).orElse(new Parking());
+            mypark.setVehicletype(park.getVehicletype() != null ? park.getVehicletype() : "name not mentioned");
             if (mypark.isStatus()) {
                 return Status.USER_ALREADY_EXISTS;
             }
@@ -40,11 +45,13 @@ class ParkingController {
             mypark.getParkuser().add(user);
             parkingRepository.save(mypark);
 
+            mypark.setParkuser(null);//garbage collection
             user.getMyparking().add(mypark);
             userparkRepository.save(user);
             return Status.UPDATE_SUCCESS;
+            }
         }
-        return Status.FAILURE;
+        return Status.UNREGISTER_USER;
     }
 
     @PostMapping("/parking/unoccupy")
@@ -52,6 +59,7 @@ class ParkingController {
         Parking mypark = parkingRepository.findById(parking.getParkid()).orElse(null);
         if(mypark==null) return Status.FAILURE;
         mypark.setStatus(false);
+        parkingRepository.save(mypark);
         return Status.UPDATE_SUCCESS;
     }
 }
